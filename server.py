f9687ee1df2e8e4e2a581cbf94e115a2e318a8f8
chuki50 <jaime0205@gmail.com>
2019-05-03 13:04:02 +0200

    BASIC finished!

--- server.py
+++ server.py
@@ -83,7 +83,10 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             r = requests.get(server + ext, headers={"Content-Type": "application/json"})
 
             data = r.json()
-            species_list = data['species']
+            species_info = data['species']
+            name_list = []
+            for x in species_info:
+                name_list.append(x['name'])
 
             kt_main = open('kt_main.html', 'r')
             kt_menu = kt_main.read()
@@ -93,29 +96,34 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             kt_contents = kt_response.read()
             kt_response.close()
 
-            if path == '/karyotype?species=':
-                f = open('ls_error.html', 'r')
+            if path == '/karyotype?specie=':
+                f = open('kt_error.html', 'r')
                 kt_contents = f.read()
                 f.close()
 
-            elif path.startswith('/karyotype?species=') and path != '/karyotype?species=':
-                species_name = path.split('=')
-                species = species_name[1]
-                print(species)
+            elif path.startswith('/karyotype?specie=') and path != '/karyotype?specie=':
+                species_r = path.split('=')
+                species_name = species_r[1]
+                print(species_name)
 
-                if species not in species_list:
-                    f = open('ls_error.html', 'r')
+                server = "http://rest.ensembl.org"
+                ext = "/info/assembly/{}?".format(species_name)
+                r2 = requests.get(server + ext, headers={"Content-Type": "application/json"})
+
+                data2 = r2.json()
+
+                if species_name not in name_list:
+                    f = open('kt_error.html', 'r')
                     kt_contents = f.read()
                     f.close()
 
-                elif species in species_list:
-                    karyotype_ind = int()
-                    for x in species_list:
-                        if x['species'] == species:
-                            karyotype_ind = x
-                            break
-                    karyotype_num = karyotype_list[karyotype_ind]
-                    kt_contents += '<p>' + 'The karyotype for the species' + str(species) + 'is:' + str(karyotype_num) + '<p>'
+                elif species_name in name_list:
+                    karyotype = ""
+                    kt_contents += '<p>' + 'The karyotype for the species ' + str(species_name) + ' is: ' + '<p>'
+                    for k in data2['karyotype']:
+                        print(k)
+                        karyotype = str(k)
+                        kt_contents += '<p>' + karyotype + '<p>'
 
             else:
                 kt_contents = kt_menu
@@ -126,10 +134,90 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(str.encode(kt_contents))
 
-        # elif path.startswith('/chromosomeLength'):
+        elif path.startswith('/chromosomeLength'):
+            server = "http://rest.ensembl.org"
+            ext = "/info/species?"
+            r = requests.get(server + ext, headers={"Content-Type": "application/json"})
 
-        # else:
-        # f = open('error', 'r')
+            data = r.json()
+            species_info = data['species']
+            name_list = []
+            for x in species_info:
+                name_list.append(x['name'])
+
+            cl_main = open('cl_main.html', 'r')
+            cl_menu = cl_main.read()
+            cl_main.close()
+
+            cl_response = open('cl_response.html', 'r')
+            cl_contents = cl_response.read()
+            cl_response.close()
+
+            if path == '/chromosomeLength?specie=&chromo=':
+                f = open('cl_error1.html', 'r')
+                cl_contents = f.read()
+                f.close()
+
+            elif path.startswith('/chromosomeLength?specie='):
+                species_r = path.split('=')
+                species_r1 = species_r[1]
+
+                chromo_r = species_r[2]
+                chromo_chosen = str(chromo_r)
+                print(chromo_chosen)
+
+                species_r2 = str(species_r1)
+                species_name = species_r2.split('&')[0]
+                print(species_name)
+
+                server = "http://rest.ensembl.org"
+                ext = "/info/assembly/{}?".format(species_name)
+                r3 = requests.get(server + ext, headers={"Content-Type": "application/json"})
+
+                data3 = r3.json()
+
+                if species_name not in name_list:
+                    f = open('kt_error.html', 'r')
+                    cl_contents = f.read()
+                    f.close()
+
+                elif species_name in name_list:
+                    chromo_len = ''
+                    for x in data3['top_level_region']:
+                        if x['name'] == chromo_chosen:
+                            chromo_len = x['length']
+                            print(chromo_len)
+                            break
+
+                    if chromo_len == '0' or chromo_len == '':
+                        f = open('cl_error2.html', 'r')
+                        cl_contents = f.read()
+                        f.close()
+
+                    else:
+                        cl_contents += '<p>' + 'The chromosome length for the chromosome ' + str(chromo_chosen)
+                        cl_contents += ' of the species ' + str(species_name) + ' is: ' + str(chromo_len) + '<p>'
+
+
+            else:
+                cl_contents = cl_menu
+
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(str.encode(cl_contents)))
+            self.end_headers()
+            self.wfile.write(str.encode(cl_contents))
+
+        else:
+            f = open('main_error.html', 'r')
+            error_msg = f.read()
+            f.close()
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(str.encode(error_msg)))
+            self.end_headers()
+            self.wfile.write(str.encode(error_msg))
+
 
 
 Handler = TestHandler
@@ -144,7 +232,7 @@ with socketserver.TCPServer(("", PORT), Handler) as httpd:
         httpd.serve_forever()
     except KeyboardInterrupt:
         print("")
-        print("Stoped by the user")
+        print("Stopped by the user")
         httpd.server_close()
 
 print("")
